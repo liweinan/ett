da06384bdc4f7200746d262e899af8adb21f8c18
Weinan Li <weli@redhat.com>
2013-02-05 00:25:35 +0800

    performance opt

--- app/controllers/toolbox_controller.rb
+++ app/controllers/toolbox_controller.rb
@@ -6,6 +6,12 @@ class ToolboxController < ApplicationController
     end
   end
 
+  def get_pac_form
+    respond_to do |format|
+      format.js
+    end
+  end
+
   def get_initial_pac_btn_switch
     respond_to do |format|
       format.js
