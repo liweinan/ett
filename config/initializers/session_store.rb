7f9f2870244556b3639112c04b09709c87b0f206
Weinan Li <weli@redhat.com>
2013-04-14 11:42:07 +0800

    prepare step

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -4,10 +4,12 @@
 # If you change this key, all old sessions will become invalid!
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.session = {
-  :key         => '_bq_session',
-  :secret      => '6340aa86376793ce58c43edcc950f63b841e93f03fc35f6511e8024d6176f08e764519b8eb8d8fddb2516bb952068c7b5cfe1c57c25f699c9fd8f9a47f820618'
-}
+#ActionController::Base.session = {
+#  :key         => '_bq_session',
+#  :secret      => '6340aa86376793ce58c43edcc950f63b841e93f03fc35f6511e8024d6176f08e764519b8eb8d8fddb2516bb952068c7b5cfe1c57c25f699c9fd8f9a47f820618'
+#}
+
+Rails.application.config.session_store :cookie_store, :key => "_bq_session"
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
