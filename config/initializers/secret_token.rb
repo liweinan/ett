827222470389178bef61f7260404864b16a1f50b
Weinan Li <weli@redhat.com>
2013-04-14 11:55:50 +0800

    initial upgrade

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bq::Application.config.secret_token = 'a7d2a75ea4439c7c72172b225691e452d0b64bf9b9b20c107ad8bae46cb077dda39a1b160e7e94d1cb3a91fb9a40d01c5b9f2db9654bbf9844c0a8ce0ef6b6b4'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,17 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying cookie session data integrity.
-# If you change this key, all old sessions will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-#ActionController::Base.session = {
-#  :key         => '_bq_session',
-#  :secret      => '6340aa86376793ce58c43edcc950f63b841e93f03fc35f6511e8024d6176f08e764519b8eb8d8fddb2516bb952068c7b5cfe1c57c25f699c9fd8f9a47f820618'
-#}
-
-Rails.application.config.session_store :cookie_store, :key => "_bq_session"
+Bq::Application.config.session_store :cookie_store, key: '_bq_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
-# (create the session table with "rake db:sessions:create")
-# ActionController::Base.session_store = :active_record_store
+# (create the session table with "rails generate session_migration")
+# Bq::Application.config.session_store :active_record_store
