2beb4f5ac31c10601f667d83b158795622ea6ba5
Weinan Li <weli@redhat.com>
2013-07-09 00:31:46 +0800

    save progress

--- app/controllers/bz_bugs_controller.rb
+++ app/controllers/bz_bugs_controller.rb
@@ -9,7 +9,7 @@ class BzBugsController < ApplicationController
 
     if params[:type] == 'create_bz'
       # create a bug in bugzilla
-      begin        
+      begin
         begin_check_param
         check_param_user(params)
         check_param_pwd(params)
@@ -17,12 +17,12 @@ class BzBugsController < ApplicationController
         end_check_param
 
         uri = URI.parse(APP_CONFIG["bz_bug_creation_url"])
-        parameters = { 'pkg' => package.name,
-                       'version' => params[:ver],
-                       'release' => package.task.target_release,
-                       'tagversion' => package.task.candidate_tag,
-                       'userid' => extract_username(params[:user]),
-                       'pwd' => params[:pwd] }
+        parameters = {'pkg' => package.name,
+                      'version' => params[:ver],
+                      'release' => package.task.target_release,
+                      'tagversion' => package.task.candidate_tag,
+                      'userid' => extract_username(params[:user]),
+                      'pwd' => params[:pwd]}
 
         parameters['see_also'] = params[:see_also] unless params[:see_also].blank?
 
@@ -44,11 +44,11 @@ class BzBugsController < ApplicationController
         @error = e
       end
     elsif params[:type] == 'create_bz_link'
-      begin        
+      begin
         begin_check_param
         check_param_user(params)
         check_param_pwd(params)
-        check_param_bz_id(params)        
+        check_param_bz_id(params)
         end_check_param
 
         bz_id = params[:bz_id].strip
@@ -57,11 +57,11 @@ class BzBugsController < ApplicationController
         #debugger
         #(rdb:2) @response.body
         #{}"{\"id\":\"333\",\"summary\":\"edquota calls /usr/bin/vi, which does not exist\",\"status\":\"CLOSED\",\"release\":\"---\",\"milestone\":\"---\"}
-        
+
         if @response.class == Net::HTTPOK
           bz_info = JSON.parse(@response.body)
-          @bz_bug = 
-            BzBug.create_from_bz_info(bz_info, package_id, current_user)
+          @bz_bug =
+              BzBug.create_from_bz_info(bz_info, package_id, current_user)
         end
       rescue => e
         @error = e
@@ -86,12 +86,31 @@ class BzBugsController < ApplicationController
   end
 
   def update
-    if params[:action].blank?    
-      bz_bug = BzBug.find(params[:id])
-      bz_bug.bz_id = params[:bz_id]
-      bz_bug.save
-    else
+    begin
+      if params[:action].blank?
+        bz_bug = BzBug.find(params[:id])
+        bz_bug.bz_id = params[:bz_id]
+        bz_bug.save
+      else
+        begin_check_param
+        check_param_user(params)
+        check_param_pwd(params)
+        end_check_param
 
+        if params[:action] == BzBug::BZ_ACTIONS[:movetoassigned]
+        end
+
+      end
+    rescue => e
+      @error = e
+    end
+
+    respond_to do |format|
+      format.js {
+        if params[:action] == BzBug::BZ_ACTIONS[:movetoassigned]
+
+        end
+      }
     end
   end
 
@@ -104,10 +123,10 @@ class BzBugsController < ApplicationController
 
       @bz_bug = BzBug.find(params[:id])
       @response = Net::HTTP.get_response(
-        URI("#{APP_CONFIG["bz_bug_query_url"]}#{@bz_bug.bz_id}.json?userid=#{extract_username(params[:user])}&pwd=#{params[:pwd]}"))
+          URI("#{APP_CONFIG["bz_bug_query_url"]}#{@bz_bug.bz_id}.json?userid=#{extract_username(params[:user])}&pwd=#{params[:pwd]}"))
 
       if @response.class == Net::HTTPOK
-        bz_info = JSON.parse(@response.body)        
+        bz_info = JSON.parse(@response.body)
         @bz_bug = BzBug.update_from_bz_info(bz_info, @bz_bug)
       end
     rescue => e
@@ -165,8 +184,8 @@ class BzBugsController < ApplicationController
   def check_param_pwd(params)
     if params[:pwd].blank?
       @err_msg << "Bugzilla account password can't be empty.\n"
-    end 
-  end   
+    end
+  end
 
   def check_param_bz_id(params)
     if params[:bz_id].blank?
@@ -177,16 +196,17 @@ class BzBugsController < ApplicationController
   def check_param_ver(params)
     if params[:ver].blank?
       @err_msg << "Package Version (ver) can't be empty.\n"
-    end  
+    end
   end
 
   def begin_check_param
     @err_msg = ''
   end
-  
+
   def end_check_param
     unless @err_msg.blank?
       raise ArgumentError, @err_msg
     end
   end
+
 end
