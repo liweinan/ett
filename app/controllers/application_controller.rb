bfcd283d26944ceb5b68f8f5701c56104fc93029
Dustin Kut Moy Cheung <dcheung@redhat.com>
2013-11-20 11:29:15 -0500

    Stop getting those warning errors about the xmlrpc setting
    - Thanks @liweinan for the fix!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,5 @@
 # Filters added to this controller apply to all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
-require "xmlrpc/client"
-require 'open-uri'
-XMLRPC::Config::ENABLE_NIL_PARSER = true
-
 class ApplicationController < ActionController::Base
 
   helper :all # include all helpers, all the time
