7edea667aecf4c787f2af978fe0505f59c5cdd18
Dustin Kut Moy Cheung <dcheung@redhat.com>
2015-03-25 13:48:01 -0400

    Fix session for Rails 4
    session used to be a 'naked' class, without inheriting active record. In
    Rails3/4, it has to.

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,3 +1,17 @@
-class Session
-  attr_accessor :email, :password
+if Rails::VERSION::STRING < "4"
+  class Session
+    attr_accessor :email, :password
+  end
+else
+  class Session
+    include ActiveModel::Conversion
+    include ActiveModel::Validations
+    extend ActiveModel::Naming
+
+    attr_accessor :email, :password
+
+    def persisted?
+      false
+    end
+  end
 end
