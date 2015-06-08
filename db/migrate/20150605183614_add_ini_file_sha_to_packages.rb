454ba2f69eed13817d38cd3266cc5e90f88325c6
Dustin Kut Moy Cheung <dcheung@redhat.com>
2015-06-08 19:44:46 -0400

    Delete 2 files added  by mistake

--- db/migrate/20150605183614_add_ini_file_sha_to_packages.rb
+++
@@ -1,9 +0,0 @@
-class AddIniFileShaToPackages < ActiveRecord::Migration
-  def self.up
-    add_column :packages, :ini_file_sha, :string
-  end
-
-  def self.down
-    remove_column :packages, :ini_file_sha
-  end
-end
