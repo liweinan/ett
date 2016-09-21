75fda46d262c5397fb2b029e346d94b0abf168a0
Dustin Kut Moy Cheung <dcheung@redhat.com>
2016-09-20 20:30:24 -0400

    fix inconsistency

--- app/models/package.rb
+++ app/models/package.rb
@@ -943,6 +943,7 @@ class Package < ActiveRecord::Base
       brew_nvr.package_id = self.id
       brew_nvr.distro = distro
       brew_nvr.nvr = self.get_brew_name(adv_tag[0].candidate_tag + '-candidate', distro)
+      brew_nvr.link = BrewService.get_brew_rpm_link(brew_nvr.nvr)
       brew_nvr.save
       brew_nvr.nvr
     else
